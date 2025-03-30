5fadbec4e2ebe211bacbaa7057eed4356ca37df4
DonizeteVida <donizetevida1998@outlook.com>
2025-03-30 17:13:19 +0200

    Blabla

--- src/fs/main.cpp
+++ src/fs/main.cpp
@@ -1,17 +1,40 @@
+#include <iostream>
 #include <Arduino.h>
-#include <SPI.h>
+#include <SD.h>
 
 void setup()
 {
-  Serial.begin(115200);
-  SPI.begin(SCK, MISO, MOSI, SS);
-}
+  delay(5000);
 
-void loop()
-{
+  Serial.begin(115200);
+  while (!SD.begin())
+  {
+    log_d("Mount failed");
+  }
+
+  delay(5000);
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
   log_d("Total heap: %d", ESP.getHeapSize());
   log_d("Free heap: %d", ESP.getFreeHeap());
   log_d("Total PSRAM: %d", ESP.getPsramSize());
   log_d("Free PSRAM: %d", ESP.getFreePsram());
-  delay(1000);
+
+  fs::File file = SD.open("/doni.txt", FILE_WRITE);
+  file.print("Hello World");
+  file.close();
+}
+
+void loop()
+{
+  fs::File file = SD.open("/doni.txt");
+  while (file.available())
+  {
+    Serial.print(file.read());
+  }
+  file.close();
+
+  delay(10000);
 }
